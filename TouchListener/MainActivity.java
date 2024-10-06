a23f271337ca1695973b5929a5bd4ba52bc9467a
leenanadkar <leena.nadkar12@gmail.com>
2024-10-06 19:37:25 +0530

    Create MainActivity.java

---
+++ TouchListener/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.touchlistener;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+
+    @SuppressLint({"MissingInflatedId", "ClickableViewAccessibility"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textDetect);
+        textView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction())
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        Toast.makeText(getApplicationContext(),"Down",Toast.LENGTH_LONG).show();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        Toast.makeText(getApplicationContext(),"Up",Toast.LENGTH_LONG).show();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        Toast.makeText(getApplicationContext(),"Moving",Toast.LENGTH_LONG).show();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
